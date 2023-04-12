d97aa8a5e9762244d824beca45a7c1875a1bd898
Peter Todd <ptr.tdd@gmail.com>
2023-04-12 17:12:03 +0100

    add text, configurable in settings, to react test.

--- src/modules/react_test.tsx
+++ src/modules/react_test.tsx
@@ -3,18 +3,24 @@ import BaseChart from "../charts/BaseChart";
 import { createRoot } from "react-dom/client";
 
 let ID = 0;
-function ReactTest({dataStore}) {
+function ReactTest({dataStore, parent}) {
   const [id] = useState(ID++);
   const [filterSize, setFilterSize] = useState(dataStore.filterSize);
+  const [text, setText] = useState(parent.config.text);
   useEffect(() => {
     const listener = () => setFilterSize(dataStore.filterSize);
     dataStore.addListener(id, listener);
-    return () => dataStore.removeListener(id, listener);
+    parent.addListener("text", (type, data) => setText(data));
+    return () => {
+      dataStore.removeListener(id, listener);
+      parent.removeListener("text");
+    }
   }, [dataStore]);
   return (
     <div>
       <h1>React Test</h1>
       <h2>filterSize: {filterSize}</h2>
+      <p>{text}</p>
     </div>
   );
 }
@@ -22,13 +28,32 @@ function ReactTest({dataStore}) {
 class ReactChart extends BaseChart {
   constructor(dataStore, div, config) {
     super(dataStore, div, config);
-    createRoot(this.contentDiv).render(<ReactTest dataStore={dataStore} />);
+    createRoot(this.contentDiv).render(<ReactTest dataStore={dataStore} parent={this} />);
+  }
+
+  getSettings() {
+    const c = this.config;
+    const settings = super.getSettings();
+    return settings.concat([
+      {
+        label:"Text",
+        type:"textbox",
+        current_value: c.text,
+        func: x => {
+          c.text = x;
+          this._callListeners("text", x);
+        }
+      }
+    ]);
   }
 }
 
 BaseChart.types["ReactChart"] = {
   "class": ReactChart,
   name: "React HelloWorld Chart",
+  init: (config, dataSource, extraControls) => {
+    config.text = "Hello World";    
+  }
 }
 
 // we rely on the side-effect of this import to register the chart type
