2753fe2814e58c07fc8775c529f34ebfb8c37d5b
Peter Todd <ptr.tdd@gmail.com>
2023-04-21 18:32:54 +0100

    try a different way of switching secure proxy

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,8 @@ dotenv.config();
 const MDV_STATIC_DIR = process.env.MDV_STATIC_DIR || resolve("../mdv_static");
 console.log("MDV_STATIC_DIR", MDV_STATIC_DIR);
 const STATIC_PROXY = process.env.MDV_STATIC_PROXY || "http://localhost:9000";
-const isProd = process.env.NODE_ENV === "production";
 
-export default defineConfig({
+export default defineConfig(env => { return {
     server: {
         headers: {
             "Cross-Origin-Embedder-Policy": "require-corp",
@@ -25,7 +24,7 @@ export default defineConfig({
         proxy: {
             "/static": {
                 target: STATIC_PROXY,
-                secure: isProd,
+                secure: env.command === "serve",
                 changeOrigin: true,
                 rewrite(path) {
                     return path.replace(/^\/static/, "");
@@ -53,4 +52,4 @@ export default defineConfig({
             include: [/\.tsx?$/, /\.jsx?$/],
         })
     ],
-})
+}})
