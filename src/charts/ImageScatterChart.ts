fb4966d5e648603d7ff21037946e13755760a8e5
Peter Todd <ptr.tdd@gmail.com>
2023-06-09 18:37:19 +0100

    attempt to add settings, not updating properly

--- src/charts/ImageScatterChart.ts
+++ src/charts/ImageScatterChart.ts
@@ -9,15 +9,20 @@ import { OrbitView } from "deck.gl/typed";
 
 // not a definitive type, but marginally better than 'any', locally for now...
 type Column = { data: Float32Array, minMax: [number, number] }
-
+let nextID = 0;
 class ImageScatterChart extends BaseChart {
     canvas: HTMLCanvasElement;
     imageArray: ImageArray;
     deck: Deck;
     dataModel: DataModel;
     progress = 0;
+    billboard = true;
+    size = 20;
+    _layer: ScatterplotExLayer;
+    id: number;
     constructor(dataStore, div, config) {
         super(dataStore, div, config);
+        this.id = nextID++;
         const canvas = this.canvas = createEl("canvas", {}, this.contentDiv);
         //const gl = canvas.getContext("webgl2"); // do we need to take care of disposing resources as well?
         const { base_url, image_key, texture_size } = config.images;
@@ -37,6 +42,7 @@ class ImageScatterChart extends BaseChart {
         });
         this.imageArray.onProgress = (n) => {
             this.progress = n;
+            this._layer.setNeedsUpdate();// attempt to force redraw
             this.doDeck();
         }
         const layers = this.getLayers();
@@ -65,8 +71,8 @@ class ImageScatterChart extends BaseChart {
     doDeck() {
         // not necessary ATM... layers should update with updateTriggers
         // that's not working, but this also isn't helping...
-        // const layers = this.getLayers();
-        // this.deck.setProps({ layers });
+        const layers = this.getLayers();
+        this.deck.setProps({ layers });
     }
     getLayers() {
         const { param } = this.config;
@@ -79,35 +85,82 @@ class ImageScatterChart extends BaseChart {
             const {minMax} = col;
             return 200*(col.data[i] - minMax[0]) / (minMax[1] - minMax[0]) - 100;
         }
-        const data = new Uint32Array(cx.data.length).fill(0).map((_, i) => i);
+        
+        const {length} = cx.data;
+        const {progress, size, billboard} = this;
+        
+        // only length is needed - trying to force more updates, 
+        // but for performance when working properly we should target updates more precisely
+        const data = {length, progress, size, billboard}; //new Uint32Array(cx.data.length+1).fill(0).map((_, i) => i);
+        //data[cx.data.length] = this.progress;//experiment wirh shallow difference to try to force update
 
         const {imageArray} = this;
         // const {getImageAspect, getImageIndex} = this.imageArray;// need to bind this
-        type K = number;
+        type K = never; //data is not iterable, so ~never will be passed to the callbacks
         const layer = new ScatterplotExLayer({
+            id: this.id,
             data,
             // radiusUnits: 'pixels', //default 'meters', also lineWidthUnits...
             // stroked: true, //TODO: make sure we can render properly with this
-            billboard: true,
+            billboard: this.billboard,
             pickable: true,
-            getImageIndex: (i: K) => imageArray.getImageIndex(i),
-            getImageAspect: (i: K) => imageArray.getImageAspect(i),
-            getPosition: (i: K) => [n(cx, i), n(cy, i), n(cz, i)],
-            getRadius: (i: K) => 20,
-            getFillColor: (i: K) => [255, 255, 255],
+            getImageIndex: (i: K, {index}) => imageArray.getImageIndex(index),
+            getImageAspect: (i: K, {index}) => imageArray.getImageAspect(index),
+            getPosition: (_: K, {index, data, target}) => {
+                const i = index as number;
+                //[n(cx, i), n(cy, i), n(cz, i)] // say no to garbage
+                target[0] = n(cx, i);
+                target[1] = n(cy, i);
+                target[2] = n(cz, i);
+                return target;
+            },
+            getRadius: 1,
+            radiusScale: this.size,
+            getFillColor: [255, 255, 255],
             imageArray,
             updateTriggers: {
                 //TODO: figure out how to get this to work...
                 getImageAspect: [this.progress],
+                // imageAspect: [this.progress],
             },
             extensions: [new ImageArrayDeckExtension()]
         });
+        this._layer = layer;
         return [layer];
     }
 
     onDataFiltered() {
         this.dataModel.updateModel();
         this.doDeck();
+    }
+
+    getSettings() {
+        return [...super.getSettings(),
+            {
+                name: "billboard",
+                label: "Billboard",
+                type: "check",
+                current_value: this.billboard,
+                func: (v) => {
+                    this.billboard = v;
+                    this.doDeck();
+                }
+            },
+            {
+                type: "slider",
+                name: "size",
+                label: "Size",
+                current_value: this.size,
+                min: 5,
+                max: 100,
+                step: 1,
+                continuous: true,
+                func: (v) => {
+                    this.size = v;
+                    this.doDeck();
+                }
+            },
+        ]
     }
 }
 
