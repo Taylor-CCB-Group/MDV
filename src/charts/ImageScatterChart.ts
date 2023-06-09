e732498cf3c96559b1e51fe5c2c157f50177f8f9
Peter Todd <ptr.tdd@gmail.com>
2023-06-09 17:08:02 +0100

    add tooltip, updateTriggers attempt

--- src/charts/ImageScatterChart.ts
+++ src/charts/ImageScatterChart.ts
@@ -7,18 +7,27 @@ import { ImageArray } from "../webgl/ImageArray.js";
 import { ScatterplotExLayer, ImageArrayDeckExtension } from '../webgl/ImageArrayDeckExtension.js';
 import { OrbitView } from "deck.gl/typed";
 
+// not a definitive type, but marginally better than 'any', locally for now...
+type Column = { data: Float32Array, minMax: [number, number] }
 
 class ImageScatterChart extends BaseChart {
     canvas: HTMLCanvasElement;
     imageArray: ImageArray;
     deck: Deck;
     dataModel: DataModel;
+    progress = 0;
     constructor(dataStore, div, config) {
         super(dataStore, div, config);
         const canvas = this.canvas = createEl("canvas", {}, this.contentDiv);
         //const gl = canvas.getContext("webgl2"); // do we need to take care of disposing resources as well?
         const { base_url, image_key, texture_size } = config.images;
         this.dataModel = new DataModel(dataStore, { autoUpdate: false });
+        
+        //---- PJT XXX::: this always trips me up... wasting too much time here...
+        this.dataModel.setColumns([...config.param, image_key, config.image_title]);
+        this.dataModel.updateModel();
+        //----------------
+
         this.imageArray = new ImageArray(dataStore, canvas, this.dataModel, {
             base_url,
             image_type: "png",
@@ -26,7 +35,10 @@ class ImageScatterChart extends BaseChart {
             width: texture_size,
             height: texture_size,
         });
-        this.imageArray.onProgress = (n) => this.doDeck();
+        this.imageArray.onProgress = (n) => {
+            this.progress = n;
+            this.doDeck();
+        }
         const layers = this.getLayers();
         const view = new OrbitView();
         this.deck = new Deck({
@@ -35,49 +47,62 @@ class ImageScatterChart extends BaseChart {
             views: [view],
             controller: true,
             initialViewState: {
-                // target: [0.5, 0.5, 0.5],
-                // zoom: 0, //0 means "one pixel is one unit", 1 scales by 2
+                // if these are not set, there is an error when first using mouse-wheel to zoom
+                target: [0, 0, 0], 
+                zoom: 0, //0 means "one pixel is one unit", 1 scales by 2
+            },
+            getTooltip: (info) => {
+                const {index, picked} = info;
+                const titleColumn = this.config.image_title;
+                // this.dataModel.updateModel();
+                const text = this.dataModel.getItemField(index, titleColumn);
+                return picked && {html: `<div>${titleColumn}: '${text}'</div>`,}
             },
             // glOptions: {},
             // parameters: {},
         });
     }
     doDeck() {
-        const layers = this.getLayers();
-        this.deck.setProps({ layers });
+        // not necessary ATM... layers should update with updateTriggers
+        // that's not working, but this also isn't helping...
+        // const layers = this.getLayers();
+        // this.deck.setProps({ layers });
     }
     getLayers() {
         const { param } = this.config;
         const {columnIndex} = this.dataStore;
-        type Column = {data: Float32Array, minMax: [number, number]}
         
         const cx = columnIndex[param[0]] as Column;
         const cy = columnIndex[param[1]] as Column;
         const cz = columnIndex[param[2]] as Column;
-        function n(col, i) {
+        function n(col: Column, i: number) {
             const {minMax} = col;
             return 200*(col.data[i] - minMax[0]) / (minMax[1] - minMax[0]) - 100;
         }
-        const data = new Array(cx.data.length).fill(0).map((_, i) => i);
+        const data = new Uint32Array(cx.data.length).fill(0).map((_, i) => i);
 
         const {imageArray} = this;
         // const {getImageAspect, getImageIndex} = this.imageArray;// need to bind this
         type K = number;
-        return [
-            new ScatterplotExLayer({
-                data,
-                // radiusUnits: 'pixels', //default 'meters', also lineWidthUnits...
-                billboard: true,
-                getImageIndex: (i: K) => imageArray.getImageIndex(i),
-                getImageAspect: (i: K) => imageArray.getImageAspect(i),
-                // getPosition: (i: K) => [cx[i], cy[i], cz[i]],
-                getPosition: (i: K) => [n(cx, i), n(cy, i), n(cz, i)],
-                getRadius: (i: K) => 20,
-                getFillColor: (i: K) => [255, 255, 255],
-                imageArray, //XXX: deck doesn't like mutating props like this (but it has a callback to update it)
-                extensions: [new ImageArrayDeckExtension()]
-            })
-        ];
+        const layer = new ScatterplotExLayer({
+            data,
+            // radiusUnits: 'pixels', //default 'meters', also lineWidthUnits...
+            // stroked: true, //TODO: make sure we can render properly with this
+            billboard: true,
+            pickable: true,
+            getImageIndex: (i: K) => imageArray.getImageIndex(i),
+            getImageAspect: (i: K) => imageArray.getImageAspect(i),
+            getPosition: (i: K) => [n(cx, i), n(cy, i), n(cz, i)],
+            getRadius: (i: K) => 20,
+            getFillColor: (i: K) => [255, 255, 255],
+            imageArray,
+            updateTriggers: {
+                //TODO: figure out how to get this to work...
+                getImageAspect: [this.progress],
+            },
+            extensions: [new ImageArrayDeckExtension()]
+        });
+        return [layer];
     }
 
     onDataFiltered() {
@@ -91,7 +116,7 @@ BaseChart.types["ImageScatterChart"] = {
     name: "Image Scatter Plot",
     required: ["images"],
     methodsUsingColumns: ["onDataFiltered", "getLayers"],
-    configEntriesUsingColumns: ["image_key"],
+    configEntriesUsingColumns: ["image_key", "image_title"],
 
     init: (config, dataSource, extraControls) => {
         //get the available images
