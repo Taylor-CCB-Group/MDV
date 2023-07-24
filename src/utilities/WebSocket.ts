f98826e432ace97aa1edb48eb423769f6f6f482a
Peter Todd <ptr.tdd@gmail.com>
2023-07-24 16:07:29 +0100

    don't wait to execute popout command

--- src/utilities/WebSocket.ts
+++ src/utilities/WebSocket.ts
@@ -12,7 +12,7 @@ export default async function connectWebsocket(url: string, cm: ChartManager) {
 
     socket.connect();
     socket.on("popout", async (chartID: string) => {
-        await new Promise((resolve) => setTimeout(resolve, 5000));
+        // await new Promise((resolve) => setTimeout(resolve, 5000));
         const chart = cm.charts[chartID];
         if (!chart) {
             console.error(`Chart ${chartID} not found`);
