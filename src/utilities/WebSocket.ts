473c19c7ece936218d5ddade18cb5092e87d5320
Peter Todd <ptr.tdd@gmail.com>
2023-07-26 14:25:12 +0100

    vuplex hack

--- src/utilities/WebSocket.ts
+++ src/utilities/WebSocket.ts
@@ -2,16 +2,34 @@ import { io } from "socket.io-client";
 import { ChartManager } from "../charts/charts";
 import { DataModel } from "../table/DataModel";
 
-export default async function connectWebsocket(url: string, cm: ChartManager) {
-    const socket = io(url);
+declare global {
+    interface Window {
+        vuplex?: {
+            postMessage: (msg: any) => void;
+            addEventListener: (type: string, func: (msg: any) => void) => void;
+        };
+    }
+}
 
+
+export default async function connectWebsocket(url: string, cm: ChartManager) {
+    setupVuplex();
+    const socket = io(url);
     const originalPopOutChart = cm._popOutChart;
     cm._popOutChart = (chart) => {
         originalPopOutChart.apply(cm, [chart]);
         socket.emit("popout", chart.config.id);
+        if (window.vuplex) window.vuplex.postMessage({ type: "popout", chartID: chart.config.id });
     }
 
     socket.connect();
+    function popout(chartID: string) {
+        const chart = cm.charts[chartID];
+        if (!chart) {
+            console.error(`Chart ${chartID} not found`);
+        }
+        cm._popOutChart(chart.chart);
+    }
     socket.on("popout", async (chartID: string) => {
         // await new Promise((resolve) => setTimeout(resolve, 5000));
         const chart = cm.charts[chartID];
@@ -29,6 +47,16 @@ export default async function connectWebsocket(url: string, cm: ChartManager) {
         dataModel.addListener('socket', () => {
             socket.emit('filter', {dataSource: ds.name, indices: dataModel.data});
         });
+    }
+    
+    function setupVuplex() {
+        if (window.vuplex) {
+            window.vuplex.addEventListener("message", (msg) => {
+                if (msg.type === "popout") {
+                    popout(msg.chartID);
+                }
+            });
+        }
     }
     
     // debugger;
