8c3288dfb8f02f18b91d562335df3f6fab3e1b4a
Peter Todd <ptr.tdd@gmail.com>
2023-09-07 15:21:59 +0100

    jupyter example

--- python/mdv_desktop.py
+++ python/mdv_desktop.py
@@ -1,3 +1,4 @@
+# from jupyterlab import labapp
 from mdv.mdvproject import MDVProject
 
 """
@@ -5,5 +6,6 @@ for now, just a placeholder for something that will be an entry point for a 'des
 i.e. something to allow users to easily explore their data via gui 
 without having to install npm/python dependencies etc.
 """
+# labapp.main()
 
 MDVProject("/Users/petertodd/data/mdv_obj_test2").serve()
