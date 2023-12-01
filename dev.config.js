268753db429570875edf5dc25e5bd02149ea210b
Martin Sergeant <m.j.sergeant1@googlemail.com>
2023-12-01 19:17:59 +0000

    allow content from a server for development

--- dev.config.js
+++ dev.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 webpack=require("webpack");
 module.exports = (env) => {
-	return {
+	const config=  {
         mode:"development",
 		
         devtool: 'inline-source-map',
@@ -9,8 +9,8 @@ module.exports = (env) => {
 			headers: {
 				"Cross-Origin-Opener-Policy":"same-origin",
 				"Cross-Origin-Embedder-Policy":"require-corp"
-			},
-			static:env.folder
+			}
+			//static:env.folder
 		  },
 	
 	   entry: "./src/modules/desktop_index.js",
@@ -24,24 +24,37 @@ module.exports = (env) => {
   	   module:{ 
 			
      		rules:[
+				{
+					test:/\.css$/,
+					use:[
+						"style-loader",
+						"css-loader"
+					]
+				},
+				{
+					test: /\.(png|svg|jpg|gif|eot|ttf|woff|woff2)$/,
+					type: 'asset/resource'
+				
+				},
+				{
+					test: /\.tsx?$/,
+					use: 'ts-loader',
+					exclude: '/node_modules'
+				}
+			]
+		},
+	}
+	if (env.folder){
+		config.devServer.static=env.folder;
+	}
+	if (env.server){
+		config.devServer.proxy= [
 			{
-				test:/\.css$/,
-				use:[
-					"style-loader",
-					"css-loader"
-				]
-			},
-			{
-				test: /\.(png|svg|jpg|gif|eot|ttf|woff|woff2)$/,
-				type: 'asset/resource'
-			
-			},
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: '/node_modules'
+				context: ['/images', '/tracks', '/get_configs', '/get_view',
+				 '/get_binary_data', '/get_row_data', '/get_data', '/save_state',],
+				target: env.server,
 			}
 		]
-  	}
-}
+	}
+	return config
 };
--- python/mdvtools/server.py
+++ python/mdvtools/server.py
@@ -10,8 +10,12 @@ import os
 
 
 def add_safe_headers(resp):
+    #headers required for web workers
     resp.headers["Cross-Origin-Opener-Policy"]= "same-origin"
     resp.headers["Cross-Origin-Embedder-Policy"]="require-corp"
+    #headers required if serving endpoints for another server e,g dev server
+    resp.headers["Access-Control-Allow-Origin"]="*"
+    resp.headers["Access-Control-Allow-Headers"]="Content-Type"
     return resp
 
 #flask send_file can't always cope with relative paths
