841537a1a0fe7f602ee94821f683bf9338254894
Peter Todd <ptr.tdd@gmail.com>
2024-02-01 17:02:00 +0000

    minor cleanup of scanpy example

--- python/mdvtools/test_projects/scanpy_pbmc3k.py
+++ python/mdvtools/test_projects/scanpy_pbmc3k.py
@@ -1,9 +1,17 @@
 from mdvtools.conversions import convert_scanpy_to_mdv
+from mdvtools.mdvproject import MDVProject
 import scanpy as sc
 import os
 
-project_folder = os.path.expanduser('~/mdv/pbmc3k')
-data = sc.datasets.pbmc3k_processed()
-p = convert_scanpy_to_mdv(project_folder, data)
+base = os.path.expanduser('~/mdv')
+project_folder = os.path.join(base, 'pbmc3k')
+if not os.path.exists(os.path.expanduser('~/mdv')):
+    os.makedirs(base)
+if not os.path.exists(project_folder):
+    data = sc.datasets.pbmc3k_processed()
+    p = convert_scanpy_to_mdv(project_folder, data)
+else:
+    print('using existing project...')
+    p = MDVProject(project_folder)
 p.set_editable(True)
 p.serve(port=5052) # port conflict locally as of writing...
