266c7cc14d7eda890eaff217b9af70458dd84883
Peter Todd <ptr.tdd@gmail.com>
2024-02-09 14:06:12 +0000

    finally starting to get channel controls done.

--- src/react/components/ColorChannelComponents.tsx
+++ src/react/components/ColorChannelComponents.tsx
@@ -1,9 +1,7 @@
 import { action } from "mobx";
-import { useChannelStats } from "../hooks";
-import { VivMDVReact } from "./VivMDVReact";
 import { observer } from "mobx-react-lite";
-import { OmeTiffProvider, useChart, useOmeTiff } from "../context";
-import { ChannelsState, VivProvider, useChannelsState, useChannelsStore, useImageSettingsStore } from "./avivatorish/state";
+import { VivProvider, useChannelsStore, useChannelsStoreApi } from "./avivatorish/state";
+import { Slider } from "@mui/material";
 
 
 export default observer(function MainVivColorDialog() {
@@ -14,8 +12,32 @@ export default observer(function MainVivColorDialog() {
     )
 })
 
+
+const ChannelSliders = ({ index }: { index: number }) => {
+    const limits = useChannelsStore(({ contrastLimits }) => contrastLimits);
+    const channelsStore = useChannelsStoreApi();
+    // not sure I want to be using material.ui... consider adding a widget abstration layer.
+    return (
+        <div>
+            <Slider
+                value={limits[index]}
+                onChange={action((e, v) => {
+                    limits[index] = v as [number, number];
+                    const contrastLimits = [...limits];
+                    channelsStore.setState({ contrastLimits });
+                })}
+            />
+        </div>
+    )
+}
+
 export const Test = () => {
     const colors = useChannelsStore(({ colors }) => colors);
+    const colorsApi = useChannelsStoreApi();
     const str = JSON.stringify(colors);
-    return <div>{str}</div>
+    return <div style={{width: '100%'}}>{
+        colors.map((c, i) => (
+            <ChannelSliders key={i} index={i} />
+        ))}
+    </div>
 }
