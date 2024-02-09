d520558f00ad52ab16cdf1a5c58b6916243d02f8
Peter Todd <ptr.tdd@gmail.com>
2024-02-09 14:40:36 +0000

    show channel names

--- src/react/components/ColorChannelComponents.tsx
+++ src/react/components/ColorChannelComponents.tsx
@@ -1,6 +1,6 @@
 import { action } from "mobx";
 import { observer } from "mobx-react-lite";
-import { VivProvider, useChannelsStore, useChannelsStoreApi } from "./avivatorish/state";
+import { VivProvider, useChannelsStore, useChannelsStoreApi, useMetadata } from "./avivatorish/state";
 import { Slider } from "@mui/material";
 
 
@@ -15,11 +15,24 @@ export default observer(function MainVivColorDialog() {
 
 const ChannelSliders = ({ index }: { index: number }) => {
     const limits = useChannelsStore(({ contrastLimits }) => contrastLimits);
+    const {colors, selections} = useChannelsStore(({ colors, selections }) => ({colors, selections}));
+    const metadata = useMetadata();
+    if (!metadata) throw 'no metadata'; //TODO type metadata
+    const name = metadata?.Pixels.Channels[selections[index].c].Name;
+    const color = colors[index];
+    const colorString = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
     const channelsStore = useChannelsStoreApi();
     // not sure I want to be using material.ui... consider adding a widget abstration layer.
+    // not jumping right in with using it for all layout etc because I don't want to be tied to it.
     return (
-        <div>
+        <div
+        style = {{
+            padding: '10px',
+        }}
+        >
+            {name}
             <Slider
+                style={{ color: colorString }}
                 value={limits[index]}
                 onChange={action((e, v) => {
                     limits[index] = v as [number, number];
