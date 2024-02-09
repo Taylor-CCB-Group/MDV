d72fb0bb3cdf696238527e7ed9c5ca23f9034bc6
Peter Todd <ptr.tdd@gmail.com>
2024-02-09 14:50:53 +0000

    add channel visibility toggle

--- src/react/components/ColorChannelComponents.tsx
+++ src/react/components/ColorChannelComponents.tsx
@@ -1,27 +1,27 @@
-import { action } from "mobx";
-import { observer } from "mobx-react-lite";
 import { VivProvider, useChannelsStore, useChannelsStoreApi, useMetadata } from "./avivatorish/state";
-import { Slider } from "@mui/material";
+import { Checkbox, Slider } from "@mui/material";
 
 
-export default observer(function MainVivColorDialog() {
+export default function MainVivColorDialog() {
     return (
         <VivProvider>
             <Test />
         </VivProvider>
     )
-})
+}
 
 
 const ChannelSliders = ({ index }: { index: number }) => {
     const limits = useChannelsStore(({ contrastLimits }) => contrastLimits);
-    const {colors, selections} = useChannelsStore(({ colors, selections }) => ({colors, selections}));
+    const {colors, selections, channelsVisible} = useChannelsStore(({ colors, selections, channelsVisible }) => ({colors, selections, channelsVisible}));
     const metadata = useMetadata();
+    const channelsStore = useChannelsStoreApi();
+    
     if (!metadata) throw 'no metadata'; //TODO type metadata
+    const channelVisible = channelsVisible[index];
     const name = metadata?.Pixels.Channels[selections[index].c].Name;
     const color = colors[index];
     const colorString = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
-    const channelsStore = useChannelsStoreApi();
     // not sure I want to be using material.ui... consider adding a widget abstration layer.
     // not jumping right in with using it for all layout etc because I don't want to be tied to it.
     return (
@@ -31,14 +31,22 @@ const ChannelSliders = ({ index }: { index: number }) => {
         }}
         >
             {name}
+            <Checkbox
+                checked={channelVisible}
+                onChange={() => {
+                    channelsVisible[index] = !channelVisible;
+                    const visible = [...channelsVisible];
+                    channelsStore.setState({ channelsVisible: visible });
+                }}
+            />
             <Slider
                 style={{ color: colorString }}
                 value={limits[index]}
-                onChange={action((e, v) => {
+                onChange={(e, v) => {
                     limits[index] = v as [number, number];
                     const contrastLimits = [...limits];
                     channelsStore.setState({ contrastLimits });
-                })}
+                }}
             />
         </div>
     )
