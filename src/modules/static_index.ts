48f1d611fb92f0831138c7ecbe0134703613bf94
Peter Todd <ptr.tdd@gmail.com>
2024-02-27 13:33:01 +0000

    correction to previous

--- src/modules/static_index.ts
+++ src/modules/static_index.ts
@@ -49,7 +49,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 // todo: change so that *only* ?dir=... is used to determine the root?
 // const flaskURL = window.location.pathname;
 const { href } = window.location;
-const flaskURL = new URL(href);//.origin;//href.substring(href.indexOf("/project"));
+const flaskURL = href//new URL(href).origin;//href.substring(href.indexOf("/project"));
 // if URLSearchParams has a 'dir' parameter, use that as the data directory.
 const urlParams = new URLSearchParams(window.location.search);
 // if we're in a popout window, ignore the dir parameter and don't load data
