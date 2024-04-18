e8e5a470f51d8ef9a7306cb6da7a312685b6d7ee
Alejandro Quijada Leyton <aquijada@dcc.uchile.cl>
2024-04-18 14:50:37 -0400

    Added configuration to enable darkMode compatibility in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'selector',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -9,4 +10,3 @@ module.exports = {
   },
   plugins: [],
 }
-
