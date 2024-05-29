e3aa6084d261c3e394646c00d386027b28ad19ff
Alejandro Quijada Leyton <aquijada@dcc.uchile.cl>
2024-05-29 16:05:57 -0400

    Modified FileUploadDialogWrapper to allow for dialog resizing.

--- src/charts/dialogs/FileUploadDialogWrapper.tsx
+++ src/charts/dialogs/FileUploadDialogWrapper.tsx
@@ -1,7 +1,8 @@
 import { createMdvPortal } from "@/react/react_utils";
 import { BaseDialog } from "../../utilities/Dialog";
 import FileUploadDialogComponent from "./FileUploadDialog";
-import { ProjectProvider} from "../../modules/ProjectContext";
+import { ProjectProvider } from "../../modules/ProjectContext";
+
 class FileUploadDialogReact extends BaseDialog {
     root: ReturnType<typeof createMdvPortal>;
 
@@ -16,9 +17,24 @@ class FileUploadDialogReact extends BaseDialog {
         );
         this.outer.classList.add("fileUploadDialog");
         if (this.dialog) {
-            this.root = createMdvPortal(<ProjectProvider><FileUploadDialogComponent onClose={() => this.close()} /></ProjectProvider>, this.dialog);
+            this.root = createMdvPortal(
+                <ProjectProvider>
+                    <FileUploadDialogComponent
+                        onClose={() => this.close()}
+                        onResize={(width: number, height: number) => this.resizeDialog(width, height)} // Pass the resize callback
+                    />
+                </ProjectProvider>,
+                this.dialog
+            );
         } else {
             console.error("Dialog element not found");
+        }
+    }
+
+    resizeDialog(width: number, height: number): void {
+        if (this.outer) {
+            this.outer.style.width = `${width}px`;
+            this.outer.style.height = `${height}px`;
         }
     }
 
