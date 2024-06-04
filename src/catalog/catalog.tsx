04b34ec3953579eeeed78cb4097c65ebf6980fdb
Peter Todd <ptr.tdd@gmail.com>
2024-06-04 11:03:02 +0100

    initial catalog react/tailwind config

---
+++ src/catalog/catalog.tsx
@@ -0,0 +1,9 @@
+import './catalog_index.css';
+
+export default function App() {
+    return (
+        <div>
+            <h1 className="bg-red-600">App TBD</h1>
+        </div>
+    );
+}
--- src/catalog/catalog_index.tsx
+++ src/catalog/catalog_index.tsx
@@ -1,11 +1,23 @@
-//console.log('this is the place to build the catalog/project browse interface...')
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./catalog";
 
+//console.log('this is the place to build the catalog/project browse interface...')
+const root = document.createElement('div');
+document.body.appendChild(root);
+ReactDOM.render(React.createElement(App, null), root);
+
+
+// this code should be moved into react in catalog.tsx...
 (async function () {
     const response = await fetch('/projects');
     const data = await response.json();
     console.log(data);
     const container = document.createElement('div');
     container.className = 'projects';
+    container.style.display = 'flex';
+    container.style.flexDirection = 'column';
+    container.style.alignItems = 'center';
     document.body.appendChild(container);
     for (const p of data) {
         const link = document.createElement('a');
