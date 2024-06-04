f3bb956779f3dab759960c14bd5de1a524942b4b
Peter Todd <ptr.tdd@gmail.com>
2024-06-04 13:17:57 +0100

    switch to createRoot

--- src/catalog/catalog_index.tsx
+++ src/catalog/catalog_index.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import App from "./catalog";
 
-//console.log('this is the place to build the catalog/project browse interface...')
-const root = document.createElement('div');
-document.body.appendChild(root);
-ReactDOM.render(React.createElement(App, null), root);
+const container = document.createElement('div');
+document.body.appendChild(container);
+const root = createRoot(container);
+root.render(<App />);
