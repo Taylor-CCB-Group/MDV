071bd193ecb7839adefa05349d27c3b8f0edb0b3
jayeshhireox <jayesh.hire@well.ox.ac.uk>
2024-06-13 17:09:06 +0100

    final deploy .sh .bat scripts

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,13 @@
 @echo off
+
+REM Function to check if Docker is installed
+:check_docker_installed
+docker -v >nul 2>&1
+if errorlevel 1 (
+    echo Error: Docker is not installed. Please install Docker before running this script.
+    exit /b 1
+)
+
 REM Function to check if Docker daemon is running
 :check_docker_daemon
 docker info >nul 2>&1
@@ -7,28 +16,34 @@ if errorlevel 1 (
     exit /b 1
 )
 
+
 REM Function to download and run docker-compose
 :run_docker_compose
 set PROD_COMPOSE_URL=https://raw.githubusercontent.com/Taylor-CCB-Group/MDV/jh-dev/docker-compose.yml
-
-echo Welcome to the MDV (Market Data Validation) application deployment script!
-
-REM Check if Docker is installed
-docker -v >nul 2>&1
+echo Downloading docker-compose.yml from %PROD_COMPOSE_URL%...
+curl -fsSL -o docker-compose.yml %PROD_COMPOSE_URL%
 if errorlevel 1 (
-    echo Error: Docker is not installed. Please install Docker before running this script.
+    echo Error: Failed to download docker-compose.yml. Check the URL and try again.
     exit /b 1
 )
+
+echo Running docker-compose with docker-compose.yml...
+docker-compose -f docker-compose.yml up -d
+if errorlevel 1 (
+    echo Error: Failed to run docker-compose. Check the configuration and try again.
+    exit /b 1
+)
+
+echo Welcome to the MDV application deployment script!
+
+REM Call function to check if Docker is installed
+call :check_docker_installed
 
 REM Call function to check Docker daemon
 call :check_docker_daemon
 
-REM Download and run docker-compose for production
-echo Downloading docker-compose.yml from %PROD_COMPOSE_URL%...
-curl -O %PROD_COMPOSE_URL%
-
-echo Running docker-compose with docker-compose.yml...
-docker-compose -f docker-compose.yml up -d
+REM Call function to download and run docker-compose
+call :run_docker_compose
 
 echo MDV application deployment completed successfully!
 exit /b 0
