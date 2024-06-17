d2a7e7e12f1d987f8f82f8c9c9efef8f32ff7e3e
peter_todd <ptr.tdd@gmail.com>
2024-06-17 12:12:26 +0100

    don't go into infinite loop

--- deploy.bat
+++ deploy.bat
@@ -2,6 +2,7 @@
 
 REM Function to check if Docker is installed
 :check_docker_installed
+echo Checking if Docker is installed...
 docker -v >nul 2>&1
 if errorlevel 1 (
     echo Error: Docker is not installed. Please install Docker before running this script.
@@ -10,6 +11,7 @@ if errorlevel 1 (
 
 REM Function to check if Docker daemon is running
 :check_docker_daemon
+echo Checking if Docker daemon is running...
 docker info >nul 2>&1
 if errorlevel 1 (
     echo Error: Docker daemon is not running. Please start Docker before running this script.
@@ -19,6 +21,7 @@ if errorlevel 1 (
 
 REM Function to download and run docker-compose
 :run_docker_compose
+echo Setting PROD_COMPOSE_URL environment variable...
 set PROD_COMPOSE_URL=https://raw.githubusercontent.com/Taylor-CCB-Group/MDV/jh-dev/docker-compose.yml
 echo Downloading docker-compose.yml from %PROD_COMPOSE_URL%...
 curl -fsSL -o docker-compose.yml %PROD_COMPOSE_URL%
@@ -37,13 +40,13 @@ if errorlevel 1 (
 echo Welcome to the MDV application deployment script!
 
 REM Call function to check if Docker is installed
-call :check_docker_installed
+@REM call :check_docker_installed
 
 REM Call function to check Docker daemon
-call :check_docker_daemon
+@REM call :check_docker_daemon
 
 REM Call function to download and run docker-compose
-call :run_docker_compose
+@REM call :run_docker_compose
 
 echo MDV application deployment completed successfully!
 exit /b 0
