cee81dfe618a5bd8a72b0256cc68f6ee79d40804
Peter Todd <ptr.tdd@gmail.com>
2024-07-10 13:13:00 +0100

    use .env for OpenAI API key

--- python/mdvtools/chat.py
+++ python/mdvtools/chat.py
@@ -1,6 +1,10 @@
 from mdvtools.mdvproject import MDVProject
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+print(f'Running ChatMDV prototype with OpenAI key {os.getenv("OPENAI_API_KEY")}')
 
 if __name__ == "__main__":
     path = os.path.expanduser("~/mdv/pbmc3k")
