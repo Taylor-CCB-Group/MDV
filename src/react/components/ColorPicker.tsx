54c64b32fd52b864283aa1bac752da38bde1ff46
Peter Todd <ptr.tdd@gmail.com>
2024-08-01 10:34:33 +0100

    fix try to avoid directly using style object literals

--- src/react/components/ColorPicker.tsx
+++ src/react/components/ColorPicker.tsx
@@ -2,24 +2,26 @@
 // https://codesandbox.io/s/opmco?file=/src/PopoverPicker.js
 // may in future want to go more in-depth with color picker component.
 
+import { useMemo } from "react";
+
 type MyRgbColor = [number, number, number];
 
 export const PopoverPicker = ({ color, onChange }: { color?: MyRgbColor, onChange?: (newColor: MyRgbColor) => void}) => {
     const [r, g, b] = color || [255, 255, 255];
 
     const backgroundColor = color ? `#${r.toString(16).padStart(2, "0")}${g.toString(16).padStart(2, "0")}${b.toString(16).padStart(2, "0")}` : "#000000";
-
+    const style = useMemo(() => ({
+        // padding: '10px',
+        border: 'none',
+        cursor: 'pointer',
+        width: '100%',
+        backgroundColor
+    }), [backgroundColor]);
     return (
         <input
             type="color"
             className="color-picker-no-border"
-            style={{ 
-                // padding: '10px',
-                border: 'none',
-                cursor: 'pointer',
-                width: '100%',
-                backgroundColor
-            }}
+            style={style}
             value={backgroundColor}
             onChange={e => {
                 const c = e.target.value;
