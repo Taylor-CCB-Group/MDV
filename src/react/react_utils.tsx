db4046c05a566298fb0672a2d9dbcde60ea1979d
Peter Todd <ptr.tdd@gmail.com>
2024-08-06 11:19:15 +0100

    Always use ProjectProvider and StrictMode for all react roots.

--- src/react/react_utils.tsx
+++ src/react/react_utils.tsx
@@ -2,7 +2,8 @@ import { createRoot } from "react-dom/client";
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { useMemo } from 'react';
+import { StrictMode, useMemo } from 'react';
+import { ProjectProvider } from "@/modules/ProjectContext";
 
 /** This makes sure any material-ui components have an appropriate theme applied.
  * Not clear that we want to use keep using material-ui, but for now it seems not to mix too badly.
@@ -27,16 +28,20 @@ function MaterialWrapper({ children }) {
 }
 
 /**
+ * It also makes sure that any common global context, styles etc are applied.
  * todo - this is a placeholder for refactoring so that there is a single react root,
  * then charts/dialogs etc will be rendered into it with portals.
  */
-
 const createMdvPortal = (component: JSX.Element, container: HTMLElement) => {
     const root = createRoot(container);
     root.render((
-        <MaterialWrapper>
-            {component}
-        </MaterialWrapper>
+        <StrictMode>
+            <MaterialWrapper>
+                <ProjectProvider>
+                    {component}
+                </ProjectProvider>
+            </MaterialWrapper>
+        </StrictMode>
     ));
     return root;
 }
