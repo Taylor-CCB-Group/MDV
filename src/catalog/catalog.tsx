52d866650e99a21a156e565029b4a7a051870213
jayeshhireox <jayesh.hire@well.ox.ac.uk>
2024-08-29 16:34:37 +0100

    exp

--- src/catalog/catalog.tsx
+++ src/catalog/catalog.tsx
@@ -35,7 +35,7 @@ const Projects = () => {
     const [filter, setFilter] = useState<string | null>(null);
     useEffect(() => {
         (async () => {
-            const response = await fetch('/projects');
+            const response = await fetch('/carroll/projects');
             const data = await response.json();
             setProjects(data);
         })();
