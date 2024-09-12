e65e0b6a7adfb5ea8ebd786d065ad8a383d7b5af
Peter Todd <ptr.tdd@gmail.com>
2024-09-12 15:17:07 +0100

    first pass 3d scatterplot

--- src/react/components/DeckScatterComponent.tsx
+++ src/react/components/DeckScatterComponent.tsx
@@ -1,21 +1,23 @@
 import DeckGL from "@deck.gl/react/typed";
-import { OrthographicView } from '@deck.gl/core';
+import { OrthographicView, OrbitView } from '@deck.gl/core';
 import { observer } from "mobx-react-lite";
 import { useChartSize, useConfig, useFilteredIndices, useParamColumns } from "../hooks";
 import { ScatterplotLayer } from "@deck.gl/layers/typed";
 import { useEffect, useId, useMemo, useState } from "react";
 import type { ScatterPlotConfig } from "../scatter_state";
 import { useChart } from "../context";
+// import { OrbitView } from "deck.gl/typed";
 // import { useScatterplotLayer } from "../scatter_state";
 
 export default observer(function DeckScatterComponent() {
     const id = useId();
     const [width, height] = useChartSize();
-    const [cx, cy] = useParamColumns();
+    const [cx, cy, cz] = useParamColumns();
     const data = useFilteredIndices();
     const config = useConfig<ScatterPlotConfig>();
-    const { opacity, radius, course_radius } = config;
-    const radiusScale = radius * course_radius;
+    const { opacity, radius, course_radius, dimension } = config;
+    const is2d = dimension === "2d";
+    const radiusScale = radius * course_radius * (is2d ? 1 : 0.01);
     const chart = useChart();
     const colorBy = (chart as any).colorBy;
 
@@ -31,13 +33,15 @@ export default observer(function DeckScatterComponent() {
         getPosition: (index, {target}) => {
             target[0] = cx.data[index];
             target[1] = cy.data[index];
+            if (cz) target[2] = cz.data[index];
             return target as [number, number];
         },
         getFillColor: colorBy ?? [255, 255, 255],
         getLineColor: [0, 0, 0],
         updateTriggers: {
             getFillColor: colorBy,
-        }
+        },
+        billboard: true,
     });
     const [viewState, setViewState] = useState<any>({
         width,
@@ -55,6 +59,7 @@ export default observer(function DeckScatterComponent() {
         let maxX = Number.NEGATIVE_INFINITY;
         let minY = Number.POSITIVE_INFINITY;
         let maxY = Number.NEGATIVE_INFINITY;
+        // todo z... probably refactor for less repetition
         for (let i = 0; i < data.length; i++) {
             const d = data[i];
             const x = cx.data[d];
@@ -78,24 +83,33 @@ export default observer(function DeckScatterComponent() {
     }, [data, cx, cy, width, height]);
 
     // we need an OrthographicView to prevent wrapping etc...
-    const view = useMemo(() => new OrthographicView({
-        id: "scatterplot-view",
-        controller: true,
-        width,
-        height,
-        x: 0,
-        y: 0,
-    }), [width, height]);
+    const view = useMemo(() => {
+        return config.dimension === "2d" ? new OrthographicView({
+            id: `scatterplot-view-${id}`,
+            controller: true,
+            width,
+            height,
+            x: 0,
+            y: 0,
+        }) : new OrbitView({
+            id: `scatterplot-view-${id}`,
+            controller: true,
+            width,
+            height,
+            x: 0,
+            y: 0,
+        });
+    }, [width, height, config.dimension, id]);
 
     return (
         <>
         <DeckGL 
             layers={[scatterplotLayer]}
             useDevicePixels={true}
-            controller={true}
+            // controller={true}
             viewState={viewState}
             views={view}
-            onViewStateChange={v => setViewState(v.viewState)}
+            onViewStateChange={v => setViewState(v.viewState)}            
         />
         </>
     );
