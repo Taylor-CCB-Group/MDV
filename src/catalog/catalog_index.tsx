9e24fe963fcb09afa592bdfe652d2fbe1787040d
Alejandro Quijada Leyton <aquijada@dcc.uchile.cl>
2024-09-23 16:08:13 -0300

    Modified catalog_index to use the new Dashboard component

--- src/catalog/catalog_index.tsx
+++ src/catalog/catalog_index.tsx
@@ -1,7 +1,8 @@
 import { createRoot } from "react-dom/client";
-import App from "./catalog";
+   import Dashboard from "./Dashboard";
+   import "./catalog_index.css";  // Make sure this line is present
 
-const container = document.createElement("div");
-document.body.appendChild(container);
-const root = createRoot(container);
-root.render(<App />);
+   const container = document.createElement("div");
+   document.body.appendChild(container);
+   const root = createRoot(container);
+   root.render(<Dashboard />);
