376a7f5b1393f18a95cf163f188966a482c82ca5
Maria Kiourlappou <mariak@Marias-MacBook-Pro.local>
2024-09-27 11:32:15 +0100

    committing changes in mdvtools/static

--- python/mdvtools/static/assets/deflate-D2Pgce_-.js
+++
@@ -1,2 +0,0 @@
-import{inflate as r}from"./pako.esm-DUjn5l1T.js";import{t}from"../js/mdv.js";class s extends t{decodeBlock(e){return r(new Uint8Array(e)).buffer}}export{s as default};
-//# sourceMappingURL=deflate-D2Pgce_-.js.map
