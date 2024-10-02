3557ebdb2d05344a474875353e4e82d40d134e8e
Peter Todd <ptr.tdd@gmail.com>
2024-10-02 15:05:29 +0100

    remove dead code

--- src/charts/dialogs/AnnotationDialogReact.tsx
+++ src/charts/dialogs/AnnotationDialogReact.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useMemo, useState } from "react";
 import type DataStore from "../../datastore/DataStore.js";
-import TagModel, { type TagColumn } from "../../table/TagModel";
+import TagModel from "../../table/TagModel";
 import { BaseDialog } from "../../utilities/Dialog.js";
 import { createMdvPortal } from "@/react/react_utils";
-import { X } from "lucide-react";
 import { observer } from "mobx-react-lite";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
@@ -12,86 +11,6 @@ import Chip from "@mui/material/Chip";
 import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import CheckBoxIcon from "@mui/icons-material/CheckBox";
 
-function MTag({ tag, tagModel }: { tag: string; tagModel: TagModel }) {
-    return (
-        <div className="bg-blue-600 p-2 m-1 rounded-lg flex justify-between items-center">
-            {tag}
-            <button type="button" onClick={() => tagModel.setTag(tag, false)}>
-                <X />
-            </button>
-        </div>
-    );
-}
-
-function TagInput({ tagModel }: { tagModel: TagModel }) {
-    return (
-        <input
-            className="p-2"
-            placeholder="annotation..."
-            onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                    if (e.currentTarget.value === "") return;
-                    tagModel.setTag(e.currentTarget.value, true);
-                    e.currentTarget.value = "";
-                }
-            }}
-        />
-    );
-}
-
-function TagViewX({
-    dataStore,
-    columnName,
-}: { dataStore: DataStore; columnName: string }) {
-    const tagModel = useMemo(
-        () => new TagModel(dataStore, columnName),
-        [dataStore, columnName],
-    );
-    const [tagList, setTagList] = useState(tagModel.getTags());
-    const [tagsInSelection, setTagsInSelection] = useState(
-        tagModel.getTagsInSelection(),
-    );
-    // we could also distinguish tags that every row in selection has vs only some rows.
-    const tagsNotInSelection = [...tagList].filter(
-        (tag) => !tagsInSelection.has(tag),
-    );
-    useEffect(() => {
-        const listener = tagModel.addListener(() => {
-            console.log("tagModel changed");
-            setTagList(tagModel.getTags());
-            setTagsInSelection(tagModel.getTagsInSelection());
-        });
-        return () => {
-            // probably not relevant now - expect old tagModel to be garbage collected.
-            // any remaining references to it are a bug.
-            tagModel.removeListener(listener);
-        };
-    }, [tagModel]);
-
-    return (
-        <>
-            {/* <ChooseColumn column={column} setColumn={setColumn} dataStore={dataStore} /> */}
-            <div className="flex flex-col m-4 gap-2">
-                <TagInput tagModel={tagModel} />
-                {/* <h2>Tags in selection:</h2> */}
-                <div className="flex flex-row">
-                    {[...tagsInSelection].map((tag) => (
-                        <MTag key={tag} tagModel={tagModel} tag={tag} />
-                    ))}
-                    <span className="saturate-0">
-                        {[...tagsNotInSelection].map((tag) => (
-                            <MTag key={tag} tagModel={tagModel} tag={tag} />
-                        ))}
-                    </span>
-                </div>
-                {/* <h2>All tags:</h2>
-            <div className="flex flex-row">
-                {[...tagList].map(tag => <MTag key={`b${tag}`} tagModel={tagModel} tag={tag}/>)}
-            </div> */}
-            </div>
-        </>
-    );
-}
 
 const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
 const checkedIcon = <CheckBoxIcon fontSize="small" />;
