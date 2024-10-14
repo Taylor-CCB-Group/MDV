ad2cfa287ee7246b8801f9988c09294827850a66
Maria Kiourlappou <maria.kiourlappou@well.ox.ac.uk>
2024-10-14 08:48:56 +0100

    adding time management in langchain_mdv

--- python/mdvtools/llm/langchain_mdv.py
+++ python/mdvtools/llm/langchain_mdv.py
@@ -1,3 +1,6 @@
+import time
+from contextlib import contextmanager
+
 # Code Generation using Retrieval Augmented Generation + LangChain
 import os
 import pandas as pd
@@ -20,6 +23,13 @@ from .templates import get_createproject_prompt_RAG, prompt_data
 from .code_manipulation import prepare_code
 from .code_execution import execute_code
 
+@contextmanager
+def time_block(name):
+    start_time = time.time()  # Setup: Start timing
+    yield  # This is where the code inside the `with` block runs
+    end_time = time.time()  # Teardown: After `with` block is done
+    print(f"Block '{name}' took {end_time - start_time:.4f} seconds")
+
 # sometimes this tries to draw with matplotlib... which causes an error that we can't catch
 # we don't control the code that's being run by the agent...
 # but we can call `matplotlib.use('Agg')` before calling the agent (and any other code that might try to draw)
@@ -35,31 +45,34 @@ mypath = os.path.dirname(__file__)
 
 
 print('# Crawl the GitHub repository to get a list of relevant file URLs')
-code_files_urls = crawl_github_repo()
+with time_block("b1: GitHub repo crawling"):
+    code_files_urls = crawl_github_repo()
 
-# Initialize an empty list to store the extracted code documents
-code_strings = []
+    # Initialize an empty list to store the extracted code documents
+    code_strings = []
 
-# populate code_strings with the code from .py & .ipynb files in code_files_urls
-for i in range(0, len(code_files_urls)):
-    if code_files_urls[i].endswith(".py"):
-        content = extract_python_code_from_py(code_files_urls[i])
-        doc = Document(page_content=content, metadata={"url": code_files_urls[i], "file_index": i})
-        code_strings.append(doc)
-    elif code_files_urls[i].endswith(".ipynb"):
-        content_ipynb = extract_python_code_from_ipynb(code_files_urls[i])
-        doc_ipynb = Document(page_content=content_ipynb, metadata={"url": code_files_urls[i], "file_index": i})
-        code_strings.append(doc_ipynb)
+    # populate code_strings with the code from .py & .ipynb files in code_files_urls
+    for i in range(0, len(code_files_urls)):
+        if code_files_urls[i].endswith(".py"):
+            content = extract_python_code_from_py(code_files_urls[i])
+            doc = Document(page_content=content, metadata={"url": code_files_urls[i], "file_index": i})
+            code_strings.append(doc)
+        elif code_files_urls[i].endswith(".ipynb"):
+            content_ipynb = extract_python_code_from_ipynb(code_files_urls[i])
+            doc_ipynb = Document(page_content=content_ipynb, metadata={"url": code_files_urls[i], "file_index": i})
+            code_strings.append(doc_ipynb)
 
 print('# Initialize a text splitter for chunking the code strings')
-text_splitter = RecursiveCharacterTextSplitter.from_language(
-    language=Language.PYTHON,  # Specify the language as Python
-    chunk_size=20000,           # Set the chunk size to 1500 characters
-    chunk_overlap=2000          # Set the chunk overlap to 150 characters
-)
 
-print('# Split the code documents into chunks using the text splitter')
-texts = text_splitter.split_documents(code_strings)
+with time_block("b2: Text splitter initialising"):
+    text_splitter = RecursiveCharacterTextSplitter.from_language(
+        language=Language.PYTHON,  # Specify the language as Python
+        chunk_size=20000,           # Set the chunk size to 1500 characters
+        chunk_overlap=2000          # Set the chunk overlap to 150 characters
+    )
+
+    print('# Split the code documents into chunks using the text splitter')
+    texts = text_splitter.split_documents(code_strings)
 
 # Set the number of queries per minute (QPM) for embedding requests
 # EMBEDDING_QPM = 100 # unused
@@ -68,19 +81,24 @@ texts = text_splitter.split_documents(code_strings)
 # EMBEDDING_NUM_BATCH = 5 # unused
 
 print('# Initialize an instance of the OpenAIEmbeddings class')
-embeddings = OpenAIEmbeddings(
-    model="text-embedding-3-large"  # Specify the model to use for generating embeddings
-    )
+with time_block("b3: Embeddings creating"):
+    embeddings = OpenAIEmbeddings(
+        model="text-embedding-3-large"  # Specify the model to use for generating embeddings
+        )
 
 print('# Create an index from the embedded code chunks')
 print('# Use FAISS (Facebook AI Similarity Search) to create a searchable index')
-db = FAISS.from_documents(texts, embeddings)
+
+with time_block("b4: FAISS database creating"):
+    db = FAISS.from_documents(texts, embeddings)
 
 print('# Initialize the retriever from the FAISS index')
-retriever = db.as_retriever(
-    search_type="similarity",      # Specify the search type as "similarity"
-    search_kwargs={"k": 5},        # Set search parameters, in this case, return the top 5 results
-)
+
+with time_block("b5: Retriever creating"):
+    retriever = db.as_retriever(
+        search_type="similarity",      # Specify the search type as "similarity"
+        search_kwargs={"k": 5},        # Set search parameters, in this case, return the top 5 results
+    )
 
 class ProjectChat():
     def __init__(self, project: MDVProject, log: Callable[[str], None] = print):
@@ -93,11 +111,14 @@ class ProjectChat():
         self.ds_name = project.datasources[0]['name']
         try:
             self.df = project.get_datasource_as_dataframe(self.ds_name)
-            self.code_llm = ChatOpenAI(temperature=0.1, model_name="gpt-4o")
-            self.dataframe_llm = ChatOpenAI(temperature=0.1, model_name="gpt-4")
-            self.agent = lp.create_pandas_dataframe_agent(
-                self.dataframe_llm, self.df, verbose=True, handle_parse_errors=True, allow_dangerous_code=True
-            )
+            with time_block("b6: Initialising LLM for RAG"):
+                self.code_llm = ChatOpenAI(temperature=0.1, model_name="gpt-4o")
+            with time_block("b7: Initialising LLM for agent"):
+                self.dataframe_llm = ChatOpenAI(temperature=0.1, model_name="gpt-4")
+            with time_block("b8: Pandas agent creating"):
+                self.agent = lp.create_pandas_dataframe_agent(
+                    self.dataframe_llm, self.df, verbose=True, handle_parse_errors=True, allow_dangerous_code=True
+                )
             self.ok = True
         except Exception as e:
             # raise ValueError(f"An error occurred while trying to create the agent: {e[:100]}")
@@ -105,38 +126,45 @@ class ProjectChat():
             self.ok = False
     
     def ask_question(self, question: str): # async?
-        self.log(f"Asking the LLM: '{question}'")
-        if not self.ok:
-            return "This agent is not ready to answer questions"
-        full_prompt = prompt_data + "\nQuestion: " + question
-        print(full_prompt)
+        with time_block("b9a: Pandas agent invoking"):
+            self.log(f"Asking the LLM: '{question}'")
+            if not self.ok:
+                return "This agent is not ready to answer questions"
+            full_prompt = prompt_data + "\nQuestion: " + question
+            print(full_prompt)
         try:
-            
-            response = self.agent.invoke(full_prompt) 
-            assert('output' in response)
+            with time_block("b9b: Pandas agent invoking"):
+                response = self.agent.invoke(full_prompt) 
+                assert('output' in response)
             #!!! csv_path is not wanted - the code tries to use that as data source name which is all wrong
-            prompt_RAG = get_createproject_prompt_RAG(self.project.id, self.ds_name, response['output'])
-            prompt_RAG_template = PromptTemplate(
-                template=prompt_RAG,
-                input_variables=["context", "question"]
-            )
-            qa_chain = RetrievalQA.from_llm(
-                llm=self.code_llm,
-                prompt=prompt_RAG_template,
-                retriever=retriever,
-                return_source_documents=True
-            )
-            context = retriever
-            output = qa_chain.invoke({"context": context, "query": question})
-            result = output["result"]
-            final_code = prepare_code(result, self.df, self.log, modify_existing_project=True, view_name=question)
+            with time_block("b10: RAG prompt preparation"):
+                prompt_RAG = get_createproject_prompt_RAG(self.project.id, self.ds_name, response['output'])
+                prompt_RAG_template = PromptTemplate(
+                    template=prompt_RAG,
+                    input_variables=["context", "question"]
+                )
+
+            with time_block("b11: RAG chain"):
+                qa_chain = RetrievalQA.from_llm(
+                    llm=self.code_llm,
+                    prompt=prompt_RAG_template,
+                    retriever=retriever,
+                    return_source_documents=True
+                )
+                context = retriever
+                output = qa_chain.invoke({"context": context, "query": question})
+                result = output["result"]
+
+            with time_block("b12: Prepare code"):
+                final_code = prepare_code(result, self.df, self.log, modify_existing_project=True, view_name=question)
             # log_to_google_sheet(sheet, str(context_information_metadata_name), output['query'], prompt_RAG, code)
             # todo - save code at various stages of processing...
             # log_chat(output, prompt_RAG, final_code)
             self.project.log_chat_item(output, prompt_RAG, final_code)
-            execute_code(final_code, open_code=False, log=self.log)
-            self.log(final_code)
-            return f"I ran some code for you:\n\n```python\n{final_code}```"
+            with time_block("b13: Execute code"):
+                execute_code(final_code, open_code=False, log=self.log)
+                self.log(final_code)
+                return f"I ran some code for you:\n\n```python\n{final_code}```"
         except Exception as e:
             return f"Error: {str(e)[:100]}"
 
@@ -153,6 +181,8 @@ def project_wizard(user_question: Optional[str], project_name: str = 'project', 
     #user_question = "Create a heatmap plot of the localisation status vs the UTR length"
     if user_question is None:
         user_question = input("What would you like to ask the LLM?")
+
+    logging.basicConfig(user_question)
 
     path_to_data = os.path.join(mypath, "sample_data/data_cells.csv")
     df = pd.read_csv(path_to_data)
