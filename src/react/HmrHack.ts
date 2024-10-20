fa0cf710dfa7cedf16aa364d501231e6120f1ec9
Peter Todd <ptr.tdd@gmail.com>
2024-10-20 20:44:02 +0100

    first proto-proto AddChartDialogReact

--- src/react/HmrHack.ts
+++ src/react/HmrHack.ts
@@ -3,6 +3,8 @@ const arr = [];
 arr.push(vSideEffect);
 import SideEffect from "../charts/dialogs/AnnotationDialogReact";
 arr.push(SideEffect);
+import addChart from "../charts/dialogs/AddChartDialogReact";
+arr.push(addChart);
 import wc from "./components/WordCloudChart";
 arr.push(wc);
 import hfc from "./components/HighlightedFeatureChart";
