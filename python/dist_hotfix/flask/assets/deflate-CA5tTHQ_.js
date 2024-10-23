30ee72aca107f5964700f88cf9239a7ad0b8a1e3
Peter Todd <ptr.tdd@gmail.com>
2024-10-23 21:11:27 +0100

    remove old build

--- python/dist_hotfix/flask/assets/deflate-CA5tTHQ_.js
+++
@@ -1,2 +0,0 @@
-import{i as r}from"./pako.esm-CPws4d4z.js";import{k as o}from"../js/mdv.js";import"./createSvgIcon-6gFOQ5xZ.js";class d extends o{decodeBlock(e){return r(new Uint8Array(e)).buffer}}export{d as default};
-//# sourceMappingURL=deflate-CA5tTHQ_.js.map
--- python/dist_hotfix/flask/assets/raw-D-xlsccP.js
+++
@@ -1,2 +0,0 @@
-import{k as o}from"../js/mdv.js";import"./createSvgIcon-6gFOQ5xZ.js";class s extends o{decodeBlock(e){return e}}export{s as default};
-//# sourceMappingURL=raw-D-xlsccP.js.map
