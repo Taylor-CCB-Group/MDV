4012dabb6a4fe0d74a7f3347d9c4d490686b633e
Peter Todd <ptr.tdd@gmail.com>
2024-10-23 21:22:18 +0100

    new build from latest frontend

---
+++ python/dist_hotfix/flask/assets/packbits-C1ej-UzG.js
@@ -0,0 +1,2 @@
+import{k as i}from"../js/mdv.js";import"./createSvgIcon-6gFOQ5xZ.js";class f extends i{decodeBlock(s){const r=new DataView(s),n=[];for(let e=0;e<s.byteLength;++e){let t=r.getInt8(e);if(t<0){const o=r.getUint8(e+1);t=-t;for(let a=0;a<=t;++a)n.push(o);e+=1}else{for(let o=0;o<=t;++o)n.push(r.getUint8(e+o+1));e+=t+1}}return new Uint8Array(n).buffer}}export{f as default};
+//# sourceMappingURL=packbits-C1ej-UzG.js.map
