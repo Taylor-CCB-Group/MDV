876b7f662a8d97011da6ebe3a4b136ad200bb2b9
Peter Todd <ptr.tdd@gmail.com>
2024-10-25 06:43:47 +0100

    some visual layout, placeholder extra_controls, state needs fixing

--- src/charts/dialogs/AddChartDialogReact.tsx
+++ src/charts/dialogs/AddChartDialogReact.tsx
@@ -14,9 +14,10 @@ import CheckBoxIcon from "@mui/icons-material/CheckBox";
 import BaseChart from "../BaseChart.js";
 import { DataStoreContext, useDataStore } from "@/react/context.js";
 import JsonView from "react18-json-view";
+import { AppBar, Box, Button, Dialog, Grid, Typography } from "@mui/material";
+import ColumnSelectionComponent from "@/react/components/ColumnSelectionComponent.js";
 
 function ChooseChartType() {
-    const [chartTypeName, setChartTypeName] = useState("");
     const dataStore = useDataStore();
     const chartTypes = useMemo(() => {
         return Object.values(BaseChart.types).filter(t => {
@@ -34,21 +35,74 @@ function ChooseChartType() {
             return true;
         });
     }, [dataStore]);
-    const chartNames = chartTypes.map(t => t.name);
+    const chartNames = chartTypes.map(t => t.name).sort((a, b) => a.localeCompare(b));
+    const [chartTypeName, setChartTypeName] = useState(chartNames[0]);
     const chartType = chartTypes.find(t => t.name === chartTypeName);
+    const extraControls = useMemo(() => {
+        return chartType?.extra_controls?.(dataStore).map((control, i) => (
+            <Box key={control.name}>
+                <Typography>{control.label}</Typography>
+                <p>
+                {control.name} :  {control.type}
+                </p>
+                <p>
+                defaultVal: {control.defaultVal}
+                </p>
+                <div>
+                    <h3>Values</h3>
+                    {control.values?.map(({name, value}) => (
+                        <Chip key={name} label={name} />
+                    ))}
+                </div>
+            </Box>
+        ));
+    }, [chartType, dataStore]);
+
     
     return (
         <>
-            <h2>Chart Type</h2>
-            <Autocomplete
-                options={chartNames}
-                value={chartTypeName}
-                onChange={(_, value) => setChartTypeName(value)}
-                renderInput={(params) => (
-                    <TextField {...params} label="Chart Type" />
-                )}
-            />
-            <JsonView src={chartType} />
+            <Grid container spacing={2} sx={{margin: 1}}>
+                <Grid item xs={6}>
+                    <Grid container direction={"column"} spacing={1}
+                    sx={{gap: 1}}
+                    >
+                        <h2>Chart Type</h2>
+                        <Autocomplete
+                            options={chartNames}
+                            value={chartTypeName}
+                            onChange={(_, value) => setChartTypeName(value)}
+                            renderInput={(params) => (
+                                <TextField {...params} label="Chart Type" />
+                            )}
+                        />
+                        <TextField label="Title" size="small" />
+                        <TextField label="Description" size="small" 
+                        multiline aria-multiline
+                        rows={6}
+                        />
+                    </Grid>
+                </Grid>
+                <Grid item xs={6}>
+                    <Grid container direction={"column"} spacing={1}
+                    sx={{gap: 1}}
+                    >
+                        <h2>Columns</h2>
+                        {chartType?.params?.map(p => (
+                            <ColumnSelectionComponent key={p.name} placeholder={p.name} 
+                            setSelectedColumn={(column) => console.log(column)}
+                            type={p.type}
+                            />
+                        ))}
+                        {extraControls}
+                        <Dialog open={false}>
+                            <JsonView src={chartType} collapsed/>
+                        </Dialog>
+                    </Grid>
+                </Grid>
+            </Grid>
+            <AppBar position="absolute" sx={{ top: 'auto', bottom: 0 }}>
+                <Button variant="contained" color="primary" disabled>Add Chart</Button>
+            </AppBar>
         </>
     );
 }
@@ -60,7 +114,9 @@ const AddChartDialogComponent = observer(
         return (
             <>
                 <DataStoreContext.Provider value={dataStore}>
+                    <div className="align-top">
                     <ChooseChartType />
+                    </div>
                 </DataStoreContext.Provider>
             </>
         )
@@ -68,24 +124,16 @@ const AddChartDialogComponent = observer(
 );
 
 class AddChartDialogReact extends BaseDialog {
-    // tagModel: TagModel; //prefer to keep this state in react... but we do need to know what the dataStore is.
-    // tagColumn: DataColumn<'text'>;
-    // dataModel: DataModel;
-    // tagListElement: HTMLDivElement;
-    // tagInput: any;
     root: ReturnType<typeof createMdvPortal>;
     constructor(dataStore: DataStore) {
         super(
             {
                 title: `Add Chart in '${dataStore.name}'`,
                 width: 400,
-                height: 300,
+                height: 420,
             },
             null,
         );
-        // this.outer.classList.add('annotationDialog');
-        // this.tagModel = new TagModel(dataStore);
-        // this.tagModel = tagModel;
         this.root = createMdvPortal(
             <AddChartDialogComponent dataStore={dataStore} />,
             this.dialog,
