b0d3dab898938ee62f181830c9601577a0f6c74c
Peter Todd <ptr.tdd@gmail.com>
2024-11-19 11:01:12 +0000

    disable broken multicolumn setting in selection dialog

--- src/react/components/SelectionDialogReact.tsx
+++ src/react/components/SelectionDialogReact.tsx
@@ -44,17 +44,19 @@ class SelectionDialogReact extends BaseReactChart<SelectionDialogConfig> {
     getSettings() {
         // todo: add settings widget for 'column' with some properties somewhat similar to params type.
         const settings = super.getSettings();
-        //@ts-expect-error seems like some dodgy jsdoc typing issue? why only here?
-        settings.push(g({
-            //!!this should be properly implemented...
-            type: "multicolumn",
-            label: "Columns To filter",
-            current_value: this.config.param,
-            func: (v) => {
-                this.config.param = v;
-                // this.removeFilter();
-            }
-        }));
+        // //@ts-expect-error seems like some dodgy jsdoc typing issue? why only here?
+        // >>> it is indeed broken at runtime - need general development of multi-column settings
+        // and how they relate to the mobx config store.
+        // settings.push(g({
+        //     //!!this should be properly implemented...
+        //     type: "multicolumn",
+        //     label: "Columns To filter",
+        //     current_value: this.config.param,
+        //     func: (v) => {
+        //         this.config.param = v;
+        //         // this.removeFilter();
+        //     }
+        // }));
         return settings;
     }
 }
