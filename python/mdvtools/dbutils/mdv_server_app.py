90d09151ba1a91aa54ab6a452689160b226a0e52
jayeshhireox <jayesh.hire@well.ox.ac.uk>
2024-12-19 19:31:05 +0000

    bia [deploy]

--- python/mdvtools/dbutils/mdv_server_app.py
+++ python/mdvtools/dbutils/mdv_server_app.py
@@ -28,7 +28,7 @@ def create_flask_app(config_name=None):
 
     
     try:
-        print("**Adding config.json details to app config")
+        print("***Adding config.json details to app config")
         load_config(app, config_name)
     except Exception as e:
         print(f"Error loading configuration: {e}")
