b288078e78868fecc8ae55784ff65f82d5da7c25
nawabfurquan <furquannawab@gmail.com>
2025-02-03 16:27:03 +0000

    fixed typing errors

--- src/datastore/rawHistogramWorker.ts
+++ src/datastore/rawHistogramWorker.ts
@@ -15,7 +15,7 @@ self.onmessage = async (event: MessageEvent<HistogramConfig>) => {
     // this logic is ok for MDV columns as of this writing
     // but what about viv raster data for example?
     const arrType = isInt32 ? Int32Array : Float32Array;
-    const dataArray = new arrType(data);
+    const dataArray = new arrType(data as any);
     const hist = new Array(bins).fill(0);
     const binWidth = (max - min) / bins;
     for (let i = 0; i < dataArray.length; i++) {
