ae26393c85258abf05ba6b52328433ae138156d8
Maria Kiourlappou <maria.kiourlappou@well.ox.ac.uk>
2025-04-04 10:10:40 +0000

    adding RAG examples for colouring by gene name

---
+++ python/mdvtools/test_projects/RAG_examples/ANNDATA_examples/dot_plot_linking.py
@@ -0,0 +1,74 @@
+import os
+import pandas as pd
+import scanpy as sc
+from mdvtools.mdvproject import MDVProject
+from mdvtools.charts.dot_plot import DotPlot
+import json 
+
+
+def create_dot_plot(title, params, size, position, colorscale):
+    """Create and configure a DotPlot instance with the given parameters."""
+    plot = DotPlot(
+        title=title,
+        params=params,
+        size=size,
+        position=position
+    )
+    plot.set_color_scale(colorscale)
+    
+    return plot
+
+def convert_plot_to_json(plot):
+    return json.loads(json.dumps(plot.plot_data, indent=2).replace("\\", ""))
+
+def main():
+    """Main function to create the project and serve it."""
+        # Constants
+    project_path = os.path.expanduser('~/mdv/project')
+    data_path = "path_to_data"
+    view_name = "default"
+    datasource_name = "datasource_name"
+    datasource_name_2 = "datasource_name_2"
+
+    # Create project
+    project = MDVProject(project_path, delete_existing=True)
+    
+    # Load data
+    adata = sc.read_h5ad(data_path)
+    data_frame_obs = pd.DataFrame(adata.obs)
+
+    data_frame_var = pd.DataFrame(adata.var)
+    data_frame_var['name'] = adata.var_names.to_list()
+
+    
+    # Add datasource
+    project.add_datasource(datasource_name, data_frame_obs)
+    project.add_datasource(datasource_name_2, data_frame_var)
+
+    # DotPlot parameters
+    param2 = "param2"
+    param2_index = data_frame_var['name'].tolist().index(param2)
+
+    # DotPlot parameters
+    dot_title = "Dot plot example title"
+    dot_params = ["param1", f"gs|{param2}(gs)|{param2_index}"]
+    dot_size = [400, 250]
+    dot_position = [10, 500]
+
+    dot_colorscale = {
+        'log': False
+    }
+    
+    # Create dot plot
+    dot_plot = create_dot_plot(dot_title, dot_params, dot_size, dot_position, dot_colorscale)
+
+    dot_plot_json = convert_plot_to_json(dot_plot)
+    
+    view_config = {'initialCharts': {datasource_name: [dot_plot_json]}}
+
+    project.set_view(view_name, view_config)
+    project.set_editable(True)
+    project.serve()
+
+if __name__ == "__main__":
+    main()
---
+++ python/mdvtools/test_projects/RAG_examples/ANNDATA_examples/scatter_plot_linking.py
@@ -0,0 +1,94 @@
+import os
+import json
+import pandas as pd
+import scanpy as sc
+from mdvtools.mdvproject import MDVProject
+from mdvtools.charts.scatter_plot import ScatterPlot
+
+def create_scatter_plot(title, params, size, position, color, brush, opacity, radius, legend_display, legend_position, xaxis_properties, yaxis_properties):
+    """Create and configure a ScatterPlot instance with the given parameters."""
+    plot = ScatterPlot(
+        title=title,
+        params=params,
+        size=size,
+        position=position
+    )
+
+    plot.set_default_color(color)
+    plot.set_brush(brush)
+    plot.set_opacity(opacity)
+    plot.set_radius(radius)
+    plot.set_color_legend(legend_display, legend_position)
+    plot.set_axis_properties("x", xaxis_properties)
+    plot.set_axis_properties("y", yaxis_properties)
+    
+    return plot
+
+def convert_plot_to_json(plot):
+    """Convert plot data to JSON format."""
+    return json.loads(json.dumps(plot.plot_data, indent=2).replace("\\\\", ""))
+
+def main():
+    """Main function to create the project and serve it."""
+    # Constants
+    project_path = os.path.expanduser('~/mdv/project')
+    data_path = "path_to_data"
+    view_name = "default"
+    datasource_name = "datasource_name"
+    datasource_name_2 = "datasource_name_2"
+    
+    # Create project
+    project = MDVProject(project_path, delete_existing=True)
+    
+    # Load data
+    adata = sc.read_h5ad(data_path)
+    data_frame_obs = pd.DataFrame(adata.obs)
+
+    data_frame_var = pd.DataFrame(adata.var)
+    data_frame_var['name'] = adata.var_names.to_list()
+    
+    # Add datasource
+    project.add_datasource(datasource_name, data_frame_obs)
+    project.add_datasource(datasource_name_2, data_frame_var)
+    
+    # ScatterPlot parameters
+    title = "Scatter Plot Example"
+    params = ["param1", "param2"] #param1 and param2 should all be a numerical variables
+    size = [792, 472]
+    position = [10, 10]
+
+    param_color = "param_color"
+    param_color_index = data_frame_var['name'].tolist().index(param_color)
+
+    color = f"gs|{param_color}(gs)|{param_color_index}"
+    brush = "poly"
+    opacity = 0.8
+    radius = 0.2
+
+    legend_display = True
+    legend_position = [375,1]
+              
+    
+    xaxis_properties = {"label": "label 1", 
+             "textSize": 13, 
+             "tickfont": 10
+    }
+
+    yaxis_properties = {"label": "label 2", 
+             "textSize": 13, 
+             "tickfont": 10
+    }
+
+    # Create plot
+    scatter_plot = create_scatter_plot(title, params, size, position, color, brush, opacity, radius, legend_display, legend_position, xaxis_properties, yaxis_properties)
+    
+    # Convert plot to JSON and set view
+    scatter_plot_json = convert_plot_to_json(scatter_plot)
+    scatterplot_view = {'initialCharts': {datasource_name: [scatter_plot_json]}}
+    
+    project.set_view(view_name, scatterplot_view)
+    project.set_editable(True)
+    project.serve()
+
+if __name__ == "__main__":
+    main()
