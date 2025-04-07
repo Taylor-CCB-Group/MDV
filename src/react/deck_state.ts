c5860f5929ef32597cbfdeec0eca8d33e771f41f
Peter Todd <ptr.tdd@gmail.com>
2025-04-07 16:08:28 +0100

    add store-agnostic useViewState hook

---
+++ src/react/deck_state.ts
@@ -0,0 +1,22 @@
+import { useViewerStore } from "./components/avivatorish/state";
+import type { DeckScatterConfig } from "./components/DeckScatterReactWrapper";
+import { useConfig } from "./hooks";
+
+
+/**
+ * we have different ways of managing view state in viv vs deck - should be unified...
+ * this is an agnostic shim for mobx vs zustand pending refactor.
+ */
+export function useViewState() {
+    // we should do something nicer than try/catch here,
+    // but longer term we'll probably have a single store/mobx state.
+    try {
+        return useViewerStore(state => state.viewState);
+    } catch (e) {
+        const config = useConfig<DeckScatterConfig>();
+        if (!config.viewState) {
+            throw new Error("No view state found in config, and no viewer store found.");
+        }
+        return config.viewState;
+    }
+}
