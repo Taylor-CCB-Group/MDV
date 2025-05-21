df5da021f6c799eae9a1329f73de50c28d9d5535
Peter Todd <ptr.tdd@gmail.com>
2025-05-21 11:28:24 +0100

    minor aria related tweaks and comments

--- src/react/components/TabHeader.tsx
+++ src/react/components/TabHeader.tsx
@@ -9,19 +9,24 @@ type TabHeaderProps<T extends string> = {
 
 const TabHeader = <T extends string>({ activeTab, setActiveTab, tabs }: TabHeaderProps<T>) => {
     const theme = useTheme();
-
+    // TODO consider re-arranging this so we have more proper association with tab panels etc.
     return (
-        <div className="w-full flex justify-around text-xs font-light">
+        <div className="w-full flex justify-around text-xs font-light" role="tablist">
             {tabs.map((tab) => (
                 <button
                     key={tab}
                     onClick={() => setActiveTab(tab)}
                     type="button"
+                    role="tab"
+                    aria-selected={activeTab === tab}
                     className={clsx(
                         "p-2 text-center border-b-2 transition-colors w-full",
-                        activeTab === tab ? "font-bold" : "font-light"
+                        // activeTab === tab ? "font-bold" : "font-light"
+                        "aria-selected:font-bold",
+                        "font-light"
                     )}
                     style={{
+                        // we should probably avoid this style object, need to review how theme related styles should be applied
                         borderColor: activeTab === tab ? theme.palette.primary.main : theme.palette.divider,
                         color: activeTab === tab
                             ? theme.palette.primary.main
