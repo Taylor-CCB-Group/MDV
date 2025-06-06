6d3efa3cfb546b0a5640f8479cf8a4bb903d7712
Peter Todd <xinaesthete@users.noreply.github.com>
2025-06-06 09:25:20 +0100

    Md-575 param setting gui operates on activeParams rather than strings (#222)
    * add a method for getting activeParams of a chart config so that concrete versions don't get accidentally included when other entries are set
    * remove method for figuring out which entries of params correspond to which parameters in ParamsSettingsGui as they can now reason more directly with them in an appropriate form

--- src/charts/DotPlot.js
+++ src/charts/DotPlot.js
@@ -24,7 +24,7 @@ class DotPlot extends SVGChart {
         console.log('setting fields for dot plot', c.param.slice(1));
         //this works ok because the method is decorated... which I think means if there's a query object,
         //`setFields` won't actually be called until the link is properly initialised
-        this.setFields(c.param.slice(1));
+        // this.setParams(c.param);
         //work out color scales
         c.color_scale = c.color_scale || { log: false };
         if (!c.color_legend) {
@@ -34,6 +34,9 @@ class DotPlot extends SVGChart {
             c.fraction_legend = { display: true };
         }
         //this.fractionScale = scaleSqrt().domain([0, 100]);
+        this.mobxAutorun(() => {
+            this._setFields(this.config.param.slice(1));
+        });
     }
     
     // removing this decorator as it muddies the waters with setParams.
@@ -41,7 +44,7 @@ class DotPlot extends SVGChart {
     //   as long as we had the workaround in `setParams()` to manually look for activeQuery
     //   but that was itself a hack.
     // @loadColumnData
-    setFields(fieldNames) {
+    _setFields(fieldNames) {
         //! we don't want to mutate the config object... 
         // we want to have a special value which signifies that it should use this behaviour.
         // then when we save state, it will have the appropriate value.
@@ -52,11 +55,12 @@ class DotPlot extends SVGChart {
         this.x_scale.domain(yLabels);
         this.onDataFiltered();
     }
-    @loadColumnData
-    setParams(params) {
-        this.config.param = params;
-        this.setFields(params.slice(1));
-    }
+    // prefering to setFields in an autorun so there's less potential for confusion
+    // @loadColumnData
+    // setParams(params) {
+    //     this.config.param = params;
+    //     this.setFields(params.slice(1));
+    // }
 
     remove(notify = true) {
         this.dim.destroy(notify);
@@ -144,6 +148,7 @@ class DotPlot extends SVGChart {
 
     onDataFiltered(dim) {
         //no need to change anything
+        //(we may want to review what isPinned means in relation to dynamic column selection - would be useful to have a way of pinning that)
         if (this.dim === dim || this.isPinned) {
             return;
         }
@@ -188,6 +193,7 @@ class DotPlot extends SVGChart {
         const cWidth = dim.width / (this.fieldNames.length);
         const fa = this.dim.filterMethod;
         this.setColorFunction();
+        // is this something we need to review to make sure that changing the category column behaves as expected?
         const vals = this.dataStore.getColumnValues(this.config.param[0]);
         const data = this.data.data.filter((x) => x.count !== 0);
         this.y_scale.domain(data.map((x) => vals[x.id]));
