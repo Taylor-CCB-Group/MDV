5e10286b33d1b9290ef1b543eb57c9ef36162e40
Peter Todd <ptr.tdd@gmail.com>
2025-07-07 17:55:03 +0000

    format project markdown in collapsible details

--- python/mdvtools/llm/markdown_utils.py
+++ python/mdvtools/llm/markdown_utils.py
@@ -1,20 +1,25 @@
 from mdvtools.mdvproject import MDVProject
 
 def create_project_markdown(project: MDVProject) -> str:
-    markdown = f"\n\n**Available Columns:**\n\n"
+    markdown = "\n\n<details>\n\n"
+    markdown += f"<summary>Project Datasources</summary>\n\n"
     for name in project.get_datasource_names():
         ds = project.get_datasource_metadata(name)
         markdown += f"**{name}:** ({ds['size']} rows)\n\n"
         columns = [(c['name'], c['datatype']) for c in ds['columns']]
         markdown += create_column_markdown(columns)
 
+    markdown += "\n\n</details>\n\n"
     return markdown
 
 def create_column_markdown(cols: list[tuple[str, str]]) -> str:
     if cols:
         markdown = "\n| Column Name | Data Type |\n|---|---|\n"
         for col in cols:
-            markdown += f"| {col[0]} | {col[1]} |\n"
+            # todo - consider more comprehensive escaping and validation
+            # particularly if we use this for e.g. LLM queries.
+            col_name = col[0].replace('|', '\\|')
+            markdown += f"| {col_name} | {col[1]} |\n"
     else:
         markdown = "No columns found."
     return f"{markdown}\n\n"
