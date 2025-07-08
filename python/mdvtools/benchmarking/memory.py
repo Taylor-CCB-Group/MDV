39202b9738397ec8f4285890edd389ffa1f6f9e3
Peter Todd <ptr.tdd@gmail.com>
2025-07-08 13:44:24 +0000

    import psutil locally

--- python/mdvtools/benchmarking/memory.py
+++ python/mdvtools/benchmarking/memory.py
@@ -1,9 +1,9 @@
-import psutil
 import os
 
 def get_memory_usage():
     """Get current memory usage in GB."""
     try:
+        import psutil
         process = psutil.Process(os.getpid())
         return process.memory_info().rss / 1024 / 1024 / 1024
     except ImportError:
