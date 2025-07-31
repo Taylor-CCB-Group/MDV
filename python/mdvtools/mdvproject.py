25b39a4c12af03548d80b029f6bb55526245e493
Peter Todd <ptr.tdd@gmail.com>
2025-07-31 15:12:54 +0100

    reapply pyright friendly mdvproject changes

--- python/mdvtools/mdvproject.py
+++ python/mdvtools/mdvproject.py
@@ -1946,16 +1946,17 @@ def add_column_to_group(
     ):
         #in pandas missing values are represented by NaN
         #which cause problems when co-ercing into text, therefore replace with ND
-        if data.dtype == "category": # type: ignore not sure what's best here
-        #if isinstance(data, pandas.CategoricalDtype):
-            data = data.cat.add_categories("ND")
+        if isinstance(data.dtype, pandas.CategoricalDtype):
+            # Handle pandas Categorical data
+            if "ND" not in data.cat.categories:
+                # see test_categorical_missing_values_edge_cases()
+                data = data.cat.add_categories("ND")
             data = data.fillna("ND")
-        #no boolean datatype at the moment have to co-erce to text
-        elif data.dtype== "boolean":
-            data= data.apply(lambda x: "True" if x is True else "False" if x is False else "ND")
         #no boolean datatype in MDV at the moment, have to co-erce to text
-        #elif is_bool_dtype(data):
-        #    data = data.apply(lambda x: "True" if x is True else "False" if x is False else "ND")
+        elif is_bool_dtype(data):
+            cat = data.apply(lambda x: "True" if x is True else "False" if x is False else "ND")
+            assert isinstance(cat, pandas.Series)
+            data = cat
         else:
             # may need to double-check this...
             data = data.fillna("ND")
