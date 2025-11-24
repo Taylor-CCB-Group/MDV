b6db46caef21a2c6d59259421fd01a4dbec7db23
Peter Todd <xinaesthete@users.noreply.github.com>
2025-11-24 18:24:26 +0000

    Avoid default_color being used as z coordinate in viv chart. Fix showJson toggle reactivity. (#290)
    * change viv config.param to only refer to coordinates, clean up some types.
    * contourParameter can fall back to undefined
    * type annotation on getColumnValues format argument
    * fix useConfig inside hook didn't observe showJson changes

--- src/datastore/DataStore.js
+++ src/datastore/DataStore.js
@@ -1808,11 +1808,17 @@ class DataStore {
     }
 
     /**
-     * @param {string} column - the column's field/id
+     * @param {string | undefined} column - the column's field/id
+     * @param {"name_value" | undefined} format - if "name_value", returns `{name: string, value: string}[]` for use in settings 'dropdown' widget
      * @returns {string[]} - the column's values
      */
     getColumnValues(column, format = null) {
+        if (column === undefined) {
+            console.warn('getColumnValues(undefined)');
+            return [];
+        }
         const v = this.columnIndex[column].values;
+        // could throw here if v is undefined (ie bad column arg)
         if (format === "name_value") {
             const ls = Array.from(v, (x) => ({ name: x, value: x })).sort(
                 (a, b) => a.name.localeCompare(b.name),
