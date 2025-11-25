a2c79701717cf7673190c7d0e2549db7c01b7b19
Peter Todd <ptr.tdd@gmail.com>
2025-11-25 10:32:09 +0000

    comment out test code

--- src/tests/ScatterDeckExtension.spec.ts
+++ src/tests/ScatterDeckExtension.spec.ts
@@ -1,59 +1,61 @@
-import { OrthographicView } from '@deck.gl/core';
-import { expect, test, vi } from 'vitest';
-import { generateLayerTests, testLayer } from '@deck.gl/test-utils';
-import { ScatterSquareExtension, ScatterDensityExension } from '../webgl/ScatterDeckExtension';
-import { type ScatterplotLayerProps, ScatterplotLayer } from "@deck.gl/layers";
+// hope to get back to this once deck test-utils config issues are figured out.
 
-interface Position {
-  x: number;
-  y: number;
-  z: number;
-}
+// import { OrthographicView } from '@deck.gl/core';
+// import { expect, test, vi } from 'vitest';
+// import { generateLayerTests, testLayer } from '@deck.gl/test-utils';
+// import { ScatterSquareExtension, ScatterDensityExension } from '../webgl/ScatterDeckExtension';
+// import { type ScatterplotLayerProps, ScatterplotLayer } from "@deck.gl/layers";
 
-test('ScatterSquareExtension', (t) => {
-  const view = new OrthographicView({
-    id: 'test',
-    width: 4,
-    height: 4,
-    // target: [2, 2, 0],
-    // zoom: 0,
-    viewState: {
-      target: [2, 2, 0],
-      zoom: 0,
-    }
-  });
-  const testCases = generateLayerTests({
-    Layer: ScatterplotLayer<Position>,
-    assert: (value, _msg) => expect(value).toBeTruthy(),
-    sampleProps: {
-      extensions: [new ScatterSquareExtension()],
-      data: [
-        { x: 0, y: 0, z: 0 },
-        { x: 1, y: 1, z: 1 },
-      ],
-      getPosition: (d: { x: number, y: number, z: number }) => [d.x, d.y, d.z] as [number, number, number],
-    },
-    onAfterUpdate: () => {
-      t.annotate('scatterplot with ScatterSquareExtension onAfterUpdate');
-    },
-  });
-  const viewport = view.makeViewport({
-    width: 4,
-    height: 4,
-    viewState: {
-      target: [2, 2, 0],
-      zoom: 0,
-    }
-  });
-  expect(viewport).not.toBeNull();
-  if (!viewport) throw new Error('viewport is null');
-  testLayer({
-    Layer: ScatterplotLayer<Position>,
-    testCases,
-    onError: err => {
-      // if (err) console.error(err);
-      // expect(err).toBeFalsy();
-    },
-    viewport,
-  })
-})
+// interface Position {
+//   x: number;
+//   y: number;
+//   z: number;
+// }
+
+// test('ScatterSquareExtension', (t) => {
+//   const view = new OrthographicView({
+//     id: 'test',
+//     width: 4,
+//     height: 4,
+//     // target: [2, 2, 0],
+//     // zoom: 0,
+//     viewState: {
+//       target: [2, 2, 0],
+//       zoom: 0,
+//     }
+//   });
+//   const testCases = generateLayerTests({
+//     Layer: ScatterplotLayer<Position>,
+//     assert: (value, _msg) => expect(value).toBeTruthy(),
+//     sampleProps: {
+//       extensions: [new ScatterSquareExtension()],
+//       data: [
+//         { x: 0, y: 0, z: 0 },
+//         { x: 1, y: 1, z: 1 },
+//       ],
+//       getPosition: (d: { x: number, y: number, z: number }) => [d.x, d.y, d.z] as [number, number, number],
+//     },
+//     onAfterUpdate: () => {
+//       t.annotate('scatterplot with ScatterSquareExtension onAfterUpdate');
+//     },
+//   });
+//   const viewport = view.makeViewport({
+//     width: 4,
+//     height: 4,
+//     viewState: {
+//       target: [2, 2, 0],
+//       zoom: 0,
+//     }
+//   });
+//   expect(viewport).not.toBeNull();
+//   if (!viewport) throw new Error('viewport is null');
+//   testLayer({
+//     Layer: ScatterplotLayer<Position>,
+//     testCases,
+//     onError: err => {
+//       // if (err) console.error(err);
+//       // expect(err).toBeFalsy();
+//     },
+//     viewport,
+//   })
+// })
