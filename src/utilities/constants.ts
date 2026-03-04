cb8118bc2a51595cb4ed155ed3e2f928a7daf54d
nawabfurquan <furquannawab@gmail.com>
2026-03-04 13:45:54 +0000

    add help button in the catalog page

--- src/utilities/constants.ts
+++ src/utilities/constants.ts
@@ -1,5 +1,7 @@
 export const MDV_EMAIL = "mdv@ndm.ox.ac.uk";
 
+export const MDV_DOC_WEBSITE = "https://mdv.ndm.ox.ac.uk/";
+
 // Report error text
 export const ALERT_ERROR_REPORT_MESSAGE = "To report this, please copy the error message above, describe the issue and send an email to";
 
@@ -12,4 +14,10 @@ export const DEBUG_JSON_REPORT_MESSAGE = [
 export const DEBUG_ERROR_REPORT_MESSAGE = [
     "To report this, please copy the error information on the top right corner, describe the issue and send an email to",
     "Please remove any sensitive information from the error details above before sending"
+];
+
+export const HELP_DIALOG_MESSAGE = [
+    "For general feedback or to report an issue, please send an email to",
+    "When reporting an issue, please describe what went wrong and include any error messages if you have them.",
+    "For more information, please check our documentation website:",
 ];
