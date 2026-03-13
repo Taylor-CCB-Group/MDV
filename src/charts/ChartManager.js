60a8cd7a74a0a5cb3ecae8238594262f3948be20
nawabfurquan <furquannawab@gmail.com>
2026-03-13 17:24:12 +0000

    create new view when a new datasource is uploaded

--- src/charts/ChartManager.js
+++ src/charts/ChartManager.js
@@ -542,7 +542,7 @@ export class ChartManager {
 
     _setUpChangeLayoutMenu(ds) {
         this.layoutMenus[ds.name] = new ContextMenu(() => {
-            const lo = this.viewData.dataSources[ds.name].layout || "absolute";
+            const lo = this.viewData.dataSources[ds.name]?.layout || "absolute";
             return [
                 {
                     text: "Absolute",
@@ -560,6 +560,7 @@ export class ChartManager {
 
     changeLayout(type, ds) {
         const view = this.viewData.dataSources[ds.name];
+        if (!view) return;
         const current = view.layout || "absolute";
         if (type === current) {
             return;
@@ -1222,7 +1223,7 @@ export class ChartManager {
                 const config = chart.getConfig();
                 const div = chart.getDiv();
                 const d = this.viewData.dataSources[chInfo.dataSource.name];
-                if (d.layout === "gridstack") {
+                if (d?.layout === "gridstack") {
                    //this is handled by gridstack now
                 } else {
                     config.position = [div.offsetLeft, div.offsetTop];
@@ -2378,7 +2379,7 @@ export class ChartManager {
         if (
             ds &&
             this.gridStack &&
-            this.viewData.dataSources[ds.name].layout === "gridstack"
+            this.viewData.dataSources[ds.name]?.layout === "gridstack"
         ) {
             this.gridStack.manageChart(chart, ds, this._inInit);
             return;
