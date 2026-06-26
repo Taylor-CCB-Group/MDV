a7d435ceba88cd710f1d273c94e172a1ce9c8ba7
Peter Todd <ptr.tdd@gmail.com>
2026-06-26 10:06:48 +0100

    update sd.js, new version enables worker by default.
    leaving `ensureChunkWorker` mechanism now with a log in case we end up having similar again in future.

--- src/react/spatialdata/ensureChunkWorker.ts
+++ src/react/spatialdata/ensureChunkWorker.ts
@@ -5,9 +5,13 @@ import {
 
 let chunkWorkerEnabled = false;
 
-/** Lazily enable zarrextra worker-pool chunk decode before spatial zarr reads. */
+/** Lazily enable zarrextra worker-pool chunk decode before spatial zarr reads.
+ * This is currently redundant but in future we may revive to pass in options
+ */
 export function ensureChunkWorker(options?: EnableWorkerChunkDecodeOptions) {
     if (chunkWorkerEnabled) return;
-    enableWorkerChunkDecode(options);
+    // we should get rid of this and let spatialdata enable by default.
+    // enableWorkerChunkDecode(options);
+    console.log("redundant call to enableWorkerChunkDecode which should now be done by default")
     chunkWorkerEnabled = true;
 }
