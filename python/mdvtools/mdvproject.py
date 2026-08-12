faf9f4ba9a362605fdef6dc16bac5f8caa9b43e0
martinSergeant <m.j.sergeant1@googlemail.com>
2026-08-12 11:57:25 +0100

    revert set_column fix

--- python/mdvtools/mdvproject.py
+++ python/mdvtools/mdvproject.py
@@ -1015,7 +1015,7 @@ class MDVProject:
                 int/numeric are converted to string (e.g. CSV-inferred cell_id).
         """
         if isinstance(column, str):
-            column = {"name": column,"field":column}
+            column = {"name": column}
         if not column.get("field"):
             column["field"] = column["name"]
         ds = self.get_datasource_metadata(datasource)
